69b789ab01f05ef9f0c23814416be43385384dd0
Cyril Duchon-Doris <Cyril.Duchon-Doris@telecom-paristech.fr>
2014-04-08 09:08:27 +0200

    Karl didn't push this one....

---
+++ src/ui/ScrollablePanel.java
@@ -0,0 +1,43 @@
+package ui;
+
+import java.awt.Dimension;
+import java.awt.Rectangle;
+
+import javax.swing.JPanel;
+import javax.swing.Scrollable;
+
+public class ScrollablePanel extends JPanel implements Scrollable {
+
+	@Override
+	public Dimension getPreferredScrollableViewportSize() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getScrollableBlockIncrement(Rectangle visibleRect,
+			int orientation, int direction) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportHeight() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean getScrollableTracksViewportWidth() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int getScrollableUnitIncrement(Rectangle visibleRect,
+			int orientation, int direction) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
