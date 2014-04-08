28bd4ad7a0751f9f7eaaad26632a4adbfa200a83
nahmani <muriel.nahmani@telecom-paristech.fr>
2014-04-08 16:31:46 +0200

    [Calibration caméra] Renvoie les coins du projecteur vus par la caméra

--- src/calibration/Calibrator.java
+++ src/calibration/Calibrator.java
@@ -1,6 +1,20 @@
 package calibration;
 
-public class Calibrator implements Runnable{
+import imageObject.ConnexeComponent;
+import imageObject.Point;
+import imageTransform.ComponentsAnalyser;
+import imageTransform.MyImage;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+
+import ui.App;
+
+public class Calibrator{
 
 	private Thread image, camera;
 	
@@ -8,13 +22,49 @@ public class Calibrator implements Runnable{
 		image = new Thread(new CalibrationProjection());
 		image.start();
 		
-		//TODO : start a thread camera
 	}
 	
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) {
 		
+		Calibrator calib = new Calibrator();
+		
+		//TODO : reconnaissance d'images
+		MyImage image;
+		try {
+			image = new MyImage(ImageIO.read(new File("Modules/image/database.test/qr_1.jpg")));
+			image.getGreyMyImage();
+			image.getBinaryMyImage();
+			
+			for (int i=0 ; i<image.getHeight() ; i++) {
+				for (int j=0 ; j>image.getWidth() ; j++) {
+					if (image.getRGB(i,j) == new Color(255,255,255).getRGB()) {
+						image.setRGB(i, j, (new Color(0, 0, 0).getRGB()));
+					}
+					else image.setRGB(i, j, (new Color(255, 255, 255).getRGB()));
+				}
+			}
+			
+			ComponentsAnalyser analyser = new ComponentsAnalyser (image);
+			ArrayList<ConnexeComponent> CClist = analyser.getCClist();
+			int k=0;
+			while (CClist.get(k).getLength() < 100) {
+				k++;
+			}
+			ConnexeComponent CalibrationSquare = CClist.get(k);
+			
+			Calibrate.CAMERA_PIXEL_LOWER_RIGHT = new Point (CalibrationSquare.getxMax(), CalibrationSquare.getyMin());
+			Calibrate.CAMERA_PIXEL_LOWER_LEFT = new Point (CalibrationSquare.getxMin(), CalibrationSquare.getyMin());
+			Calibrate.CAMERA_PIXEL_UPPER_LEFT = new Point (CalibrationSquare.getxMin(), CalibrationSquare.getyMax());
+			Calibrate.CAMERA_PIXEL_UPPER_RIGHT = new Point (CalibrationSquare.getxMax(), CalibrationSquare.getyMax());
+		} 
+		
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+			
+		//TODO : close image
+		App app = new App();
 	}
 
 }
