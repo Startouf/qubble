6f3b84cd04212d6ba88c17739ef1efda83cc6e0c
Eric <eric.masseran@telecom-paristech.fr>
2014-06-15 18:43:40 +0200

    Création d'un carré parfait comme repère : Valeurs récupérées de l'algorithme appliqué sur un carré de 200 de côté construit avec Gimp

--- squareExtract.py
+++ squareExtract.py
@@ -1,11 +1,16 @@
-with open('signature1.txt', 'r') as fichier:
+#!/usr/bin/python2.7
+
+with open('signature2.txt', 'r') as fichier:
 	with open('perfect.txt', 'w') as resultat:
 		ecrit = ''
 		contenu = fichier.read()
 		lignes = contenu.split("\n")
-		i = 0
+		i = -2
 		for ligne in lignes:
-			ecrit = "perfectSquare[{0}] = {1}\n".format(i, ligne[0:ligne.find(" ##")])
+			ecrit = ecrit + "perfectSquare[{0}] = (float) {1};\n".format(i, ligne[0:ligne.find(" ##")])
+			i = i + 1
+			ecrit = ecrit + "perfectSquare[{0}] = (float) {1};\n".format(i, ligne[0:ligne.find(" ##")])
+			i = i + 1
 		resultat.write(ecrit)
 			
 
